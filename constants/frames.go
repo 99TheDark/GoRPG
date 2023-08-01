87123c9e79d4127c873ffcd7a642d274792817cd
99TheDark <99TheDark@gmail.com>
2023-08-01 12:34:44 -0400

    Now the up-right animation is done :D — I feel like the hair end looks weird on her tho

--- constants/frames.go
+++ constants/frames.go
@@ -33,9 +33,9 @@ var PlayerAnimation = map[Direction]([]string){
 		"olivia/down_left_3.png",
 	},
 	UpRight: []string{
-		"olivia/down_1.png",
-		"olivia/down_2.png",
-		"olivia/down_3.png",
+		"olivia/up_right_1.png",
+		"olivia/up_right_2.png",
+		"olivia/up_right_3.png",
 	},
 	DownRight: []string{
 		"olivia/down_right_1.png",
