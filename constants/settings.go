46ed49e6452a515c76b5edbcf4a1d92555cc296f
99TheDark <99TheDark@gmail.com>
2023-08-01 18:09:19 -0400

    Scaled down

--- constants/settings.go
+++ constants/settings.go
@@ -3,7 +3,7 @@ package constants
 const (
 	TileSize     int     = 16   // Size of each tile in pixels
 	TileBuffer   int     = 2    // Number of tiles outside of the screen to be calculated / rendered
-	Scale        float64 = 5    // Tile scale multiplier
+	Scale        float64 = 4    // Tile scale multiplier
 	ScreenWidth  int     = 640  // Width of window
 	ScreenHeight int     = 480  // Height of window
 	PlayerSpeed  float64 = 0.06 // Speed of the player
